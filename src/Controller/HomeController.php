7775ca366f79ca75ed659e1c27e5684f98f443d6
SDdylan <38325425+SDdylan@users.noreply.github.com>
2023-01-12 18:29:46 +0100

    added webpack encore for a custom css Modified base.html.twig & index.html.twig for the homepage display Added possibility to sort by id, name or type in the homepage (need button for the user) Added route to details pokemon Modified css sheet

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -17,14 +17,40 @@ class HomeController extends AbstractController
      */
     public function index(CallApiService $callApiService): Response
     {
+        $tri = 'id';
+
         //test du fonctionnement de la récupération de données sur l'API
         $pokemons = $callApiService->getGenerationPokemonData();
+
+        //On regarde par quel paramètres on tri les pokémons
+        if (isset($_GET['tri'])) {
+            if ($_GET['tri']=='types') {
+                $tri = $_GET['tri'];
+            } elseif ($_GET['tri']=='name') {
+                $tri = $_GET['tri'];
+            } else {
+                $tri = 'id';
+            }
+        }
         //Faire en sorte que le tri puisse être choisi avec un bouton
-        $pokemonsSorted = $callApiService->sortGenerationPokemon($pokemons, 'types');
+        $pokemonsSorted = $callApiService->sortGenerationPokemon($pokemons, $tri);
 
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'generation' => $_ENV['POKEMON_GENERATION'],
             'pokemonsSorted' => $pokemonsSorted
+        ]);
+    }
+
+    /**
+     * @Route("/pokemon/{idPokemon}", name="app_pokemon_detail")
+     */
+    public function showPokemon(CallApiService $callApiService, int $idPokemon): Response
+    {
+        $pokemon = $callApiService->getPokemonData($idPokemon);
+
+        return $this->render('home/detail.html.twig', [
+            'generation' => $_ENV['POKEMON_GENERATION'],
+            'pokemon' => $pokemon
         ]);
     }
 }
